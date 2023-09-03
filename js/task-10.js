d8a96f7f3e2a5f51aec4f87a7be03878ef4978d8
Agata Wajda <agawaj98@gmail.com>
2023-09-03 14:58:36 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+const create = input.nextElementSibling;
+const destroy = create.nextElementSibling;
+const boxes = document.querySelector("#boxes");
+const items = [];
+
+function createBoxes(value) {
+  let size = 30;
+  for (let i = 0; i < value; i++) {
+    const box = document.createElement("div");
+    boxes.append(box);
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    let color = getRandomHexColor();
+    box.style.background = color;
+    size = size + 10;
+  }
+}
+
+create.addEventListener("click", () => {
+  let value = input.value;
+  createBoxes(value);
+});
+
+destroy.addEventListener("click", () => {
+  boxes.innerHTML = "";
+});
