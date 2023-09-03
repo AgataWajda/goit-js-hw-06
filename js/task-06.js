c88988dac228821e1fddf565c376b5648010c8c8
Agata Wajda <agawaj98@gmail.com>
2023-09-03 13:14:17 +0200

    task-06

---
+++ js/task-06.js
@@ -0,0 +1,11 @@
+const input = document.querySelector("#validation-input");
+
+input.addEventListener("blur", (event) => {
+  if (event.currentTarget.value.length === 6) {
+    event.currentTarget.classList.remove("invalid");
+    event.currentTarget.classList.add("valid");
+  } else {
+    event.currentTarget.classList.remove("valid");
+    event.currentTarget.classList.add("invalid");
+  }
+});
