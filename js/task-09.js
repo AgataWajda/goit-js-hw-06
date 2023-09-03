8478e0e06ef84fb4aebd416b9bb9fd66714baeae
Agata Wajda <agawaj98@gmail.com>
2023-09-03 14:03:12 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const background = document.body;
+const color = document.querySelector(".color");
+const changeBtn = document.querySelector(".change-color");
+
+changeBtn.addEventListener("click", () => {
+  let value = getRandomHexColor();
+  background.style.background = value;
+  color.textContent = value;
+});
