bb9ceea1ff1de86d3d26113258bfc70afe55808b
Agata Wajda <agawaj98@gmail.com>
2023-09-03 13:51:16 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (password === "" || email === "") {
+    alert("Please fill in all the fields!");
+  } else {
+    const user = {
+      email: email,
+      password: password,
+    };
+    console.log(user);
+    form.reset();
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
